be77be5848ad605f1422f55bb9264b5e2003e20b
aysnopye <Babalolafestus28@gmail.com>
2023-11-15 08:11:31 -0800

    error.c my first error.c

--- errors.c
+++ errors.c
@@ -3,7 +3,6 @@
 /**
  *_eputs - prints an input string
  * @str: the string to be printed
- *
  * Return: Nothing
  */
 void _eputs(char *str)
@@ -22,7 +21,6 @@ void _eputs(char *str)
 /**
  * _eputchar - writes the character c to stderr
  * @c: The character to print
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
@@ -45,7 +43,6 @@ int _eputchar(char c)
  * _putfd - writes the character c to given fd
  * @c: The character to print
  * @fd: The file descriptor to write to
- *
  * Return: On success 1.
  * On error, -1 is returned, and errno is set appropriately.
  */
